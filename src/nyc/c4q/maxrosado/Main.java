67bc7723f25559eca649574e4969beae3aac5df7
Max Rosado <max.a.rosado@gmail.com>
2016-11-13 17:14:19 -0500

    another commit

---
+++ src/nyc/c4q/maxrosado/Main.java
@@ -0,0 +1,32 @@
+package nyc.c4q.maxrosado;
+
+/**
+ * LIGHT EM UP comments
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int min = 0;
+        int max = target - 1;
+        int guess = max + min / 2;
+//        while (guess != target || max != -1)
+//            if (array[guess] == target) {
+//                return guess;
+//            } else if (array[guess] < target) {
+//                min = guess;
+//
+//            } else if (array[guess] > target) {
+//
+//            }
+//        }
+        
+
+
+        return -1;
+    };
+}
